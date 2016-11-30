e3d1d421ed7697297e39ce372c8eae411ab377a0
Zachary Bennett <zbennett10@hotmail.com>
2016-11-30 16:41:14 +0000

    Set up environment

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 //----------------------------Configuration
 var router = express();
 var server = http.createServer(router);
+router.set('view engine', 'pug');
+router.set('views', path.join(__dirname, 'views'));
+
 
 //------------------------------Middleware
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
 
 
 //-----------------------------Routes
 
-router.use(function(req,res) {
-  
+router.get(('/'),function(req,res) {
+  res.render('view');
+});
+
+router.get(('/:input'), function(req, res){
+    
 });
 
 
