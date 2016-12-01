b8daa7231a46f615bf9df54a8207958de99a631e
Zachary Bennett <zbennett10@hotmail.com>
2016-12-01 18:08:43 +0000

    minor updates

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ router.get(('/'),function(req,res) {
 
 
 
-router.get(':input', function(req, res){
+router.get('/:input', function(req, res){
     // Regex from https://gist.github.com/dperini/729294
     var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     if(!regex.test(req.params.input)) {
@@ -55,9 +55,8 @@ router.get(':input', function(req, res){
         if (doc) {
             console.log("already exists!");
          //res.json({url: "localhost/" + doc.id, old: req.params.input});
-         return router.use(function(req, res, next) {
-    res.redirect('/');
-});
+         return res.redirect(doc.url);
+
         }
         else {
             console.log("doesn't exist!");
