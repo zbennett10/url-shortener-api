2ef3a124b524e12394cfe296ac0e8af729148745
zbennett10 <zbennett10@Hotmail.com>
2016-12-10 13:10:01 -0500

    refactor

--- app.js
+++ app.js
@@ -28,22 +28,24 @@ router.get(('/'),function(req,res) {
 
 //route for url query
 router.get('/:input(*)', function(req, res){
-    // Regex from https://gist.github.com/dperini/729294
-    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-
-    if(!regex.test(req.params.input) && req.params.input.length !== 4) { //if query isn't a url or is not a 4 number sequence
-        console.log("not a valid url");
-        res.json({request_error: "Invalid url. Please refine your request."});
-        return;
-    }
+    validteRequest(req, res); //validate url
     
-    //open database and get responses
     MongoClient.connect(MONGODB_URI, function(err, db) {  //database connection
        if(err) dbErrorHandle(error, res);
        else if(findExistingUrl(res, req, db)) return;
        else createNewUrl(res, req, db);
     });
 });
+
+function validateRequest(request, response) {
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    if(!regex.test(request.params.input) && request.params.input.length !== 4) {
+        console.log("Invalid url request");
+        response.json({request_error: "Invalid url. Please refine your request."});
+        return;
+    }
+}
 
 //generates a new url
 function createNewUrl(response, request, db) {
@@ -92,5 +94,6 @@ function dbErrorHandle(error, response) {
     console.log(error);
     response.json({database_error: "There was an error processing your request. Please try again."});
 }
+
 
 module.exports = router;
